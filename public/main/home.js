1ad25fc46d4a5d27e9229bb7ff2cbc66c340556d
Bill Kuo <billkuo08@gmail.com>
2022-12-09 17:02:57 -0800

    456

--- public/main/home.js
+++ public/main/home.js
@@ -34,18 +34,18 @@ const showListOfPosts = async () => {
 
   // }
 
-  var tblBody = document.createElement("tbody");
+  let tblBody = document.createElement("tbody");
   // creates a <tbody> element
-  for (var i = 0; i < finalOutput.data.length; i++) {
+  for (let i = 0; i < finalOutput.data.length; i++) {
     // creates a table row
-    var row = document.createElement("tr");
-    for (var prop in finalOutput.data[i]) {
+    let row = document.createElement("tr");
+    for (let prop in finalOutput.data[i]) {
       // Create a <td> element and a text node, make the text
       // node the contents of the <td>, and put the <td> at
       // the end of the table row
-      var cell = document.createElement("td");
+      let cell = document.createElement("td");
       if (prop === "expenseOrIncome" || prop === "amount") {
-        var cellText = document.createTextNode(finalOutput.data[i][prop]);
+        let cellText = document.createTextNode(finalOutput.data[i][prop]);
         cell.appendChild(cellText);
         row.appendChild(cell);
       }
