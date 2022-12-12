574d558b4943fe02885043bd96d05c13a0a36e2e
Gian Viterbo <000465953@student.vcc.ca>
2022-12-12 15:35:53 -0800

    started implementing an updateUser function

--- public/settings.js
+++ public/settings.js
@@ -18,4 +18,42 @@ const showUserInfo = () => {
 
 showUserInfo();
 
+const updateName = (event) => {
+  user.name = event.target.value;
+}
+
+const updateEmail = (event) => {
+  user.email = event.target.value;
+}
+
+const updatePassword = (event) => {
+  user.password = event.target.value;
+}
+
+const submitUserForm = async (event) => {
+
+  // We don't want to page to refresh because we are not using actions here!
+  event.preventDefault();
+  console.log(user);
+  // We will call the API for our own backend Here!
+
+  try {
+    const response = await fetch(`${baseUrl}/users/update`, {
+      method: "put",
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+
+    // We have to fix this 
+    if (response) {
+      window.location.href = "login.html";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
 
