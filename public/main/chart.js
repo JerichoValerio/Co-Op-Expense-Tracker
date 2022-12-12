19176c8aaf2b5a86d3a9d2e9b61f3da2e1c5dd0a
Bill Kuo <billkuo08@gmail.com>
2022-12-11 18:44:39 -0800

    chartjs based on the database, two bugs left to be fixed

--- public/main/chart.js
+++ public/main/chart.js
@@ -1,33 +1,79 @@
 
-// const response = await fetch(`${baseUrl}/posts`);
-// const finalOutput = await response.json();
+
+const chartCreate = async () => {
+  let baseUrl = "/api/v1";
+  const response = await fetch(`${baseUrl}/posts`);
+  //const response = await fetch("http://localhost:4000/api/v1/posts");
+  const finalOutput = await response.json();
 
 
+  let xValues = [];
+  let yValues = [];
+  let barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
+
+  for (let i = 0; i < finalOutput.data.length; i++) {
+    for (let prop in finalOutput.data[i]) {
+      if (prop === "expenseOrIncome") {
+        xValues.push(finalOutput.data[i][prop])
+
+      }
+    }
+
+  }
+  console.log(xValues);
 
 
-let xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-let yValues = [55, 49, 44, 24, 15];
-let barColors = [
-  "#b91d47",
-  "#00aba9",
-  "#2b5797",
-  "#e8c3b9",
-  "#1e7145"
-];
+  let totalExpense = 0;
+  //let singleExpense = 0;
+  let percentage = 0;
+  for (let i = 0; i < finalOutput.data.length; i++) {
+    for (let totalValue in finalOutput.data[i]) {
+      if (totalValue === "amount" && finalOutput.data[i][totalValue] < 0) {
+        totalExpense += Math.abs(finalOutput.data[i][totalValue]);
+      }
+    }
+    for (let value in finalOutput.data[i]) {
 
-new Chart("myChart", {
-  type: "pie",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: "World Wide Wine Production 2018"
+      if (value === "amount" && finalOutput.data[i][value] < 0) {
+        //singleExpense += Math.abs(finalOutput.data[i][value]);
+        percentage = (Math.abs(finalOutput.data[i][value]) / totalExpense * 100).toFixed(0);
+        yValues.push(percentage);
+        //singleExpense = 0;
+        percentage = 0;
+      }
     }
   }
-});
+
+
+  console.log(yValues);
+
+
+  new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Expneses of the current month"
+      }
+    }
+  });
+
+}
+
+chartCreate();
+
+
+
